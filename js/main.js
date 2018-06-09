e4dff3008474863f1fcdc2afdb2b1efa36990306
Muhammadbilalyousuf <sp14bs0018@maju.edu.pk>
2018-06-09 21:22:47 +0500

    Added Index.html

---
+++ js/main.js
@@ -0,0 +1,11 @@
+slidr.create('imgrotate', {
+  breadcrumbs: true,
+  controls: 'border',
+  direction: 'horizontal',
+  fade: false,
+  keyboard: true,
+  overflow: true,
+  theme: '#ffffff',
+  touch: true,
+  transition: 'fade' // fade, cube, or linear
+}).start();
